7446a9e40204847b333984e4657d3085c91fd6cf
borkovacd <borkovac.dragan@uns.ac.rs>
2020-08-25 14:20:56 +0200

    Home Page -> Quick Search (Responsiveness)

--- matchmaker/src/components/forms/QuickSearchForm.js
+++ matchmaker/src/components/forms/QuickSearchForm.js
@@ -20,7 +20,7 @@ class QuickSearch extends Page {
     this.state = {
       search: "",
       result: undefined,
-      resultCount: 0,
+      resultCount: 0
     };
 
     this.doSearch = this.doSearch.bind(this);
@@ -33,7 +33,7 @@ class QuickSearch extends Page {
     searchData[event.target.name] = event.target.value;
 
     this.setState({
-      searchData: searchData,
+      searchData: searchData
     });
   }
 
@@ -46,113 +46,123 @@ class QuickSearch extends Page {
       <form id="quick-search-form" className="quick-search-form-container">
         <div className="qs-title">{strings.quickSearch.title}</div>
 
-        <div className="form-row mb-5">
-          <div className="column column-half ">
-            <div className="label">
-              <label htmlFor="gender">{strings.quickSearch.gender}</label>
+        <div className="container">
+          <div>
+            <div className="form-row mb-5">
+              <div className="column column-half ">
+                <div className="label">
+                  <label htmlFor="gender">{strings.quickSearch.gender}</label>
+                </div>
+              </div>
+              <div id="gender-select" className="column column-half">
+                <div className="language-switcher">
+                  <Select
+                    name="gender"
+                    items={getGenderList()}
+                    selectedItem={this.state.searchData.gender}
+                    displayKey={"name"}
+                    valueKey={"value"}
+                    onChange={this.onChange}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div id="gender-select" className="column column-half">
-            <div className="language-switcher">
-              <Select
-                name="gender"
-                items={getGenderList()}
-                selectedItem={this.state.searchData.gender}
-                displayKey={"name"}
-                valueKey={"value"}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="form-row mb-5">
-          <div className="column column-half">
-            <div className="label">
-              <label htmlFor="intrestedInGender">
-                {strings.quickSearch.intrestedInGender}
-              </label>
-            </div>
-          </div>
+            <div className="form-row mb-5">
+              <div className="column column-half">
+                <div className="label">
+                  <label htmlFor="intrestedInGender">
+                    {strings.quickSearch.intrestedInGender}
+                  </label>
+                </div>
+              </div>
 
-          <div id="iigender-select" className="column column-half">
-            <div className="language-switcher">
-              <Select
-                name="interestedInGender"
-                items={getGenderList()}
-                selectedItem={this.state.searchData.interestedInGender}
-                displayKey={"name"}
-                valueKey={"value"}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="form-row mb-5">
-          <div className="column column-half">
-            <div className="label">
-              <label htmlFor="betweenAges-min">
-                {strings.quickSearch.intrestedInAge}
-              </label>
+              <div id="iigender-select" className="column column-half">
+                <div className="language-switcher">
+                  <Select
+                    name="interestedInGender"
+                    items={getGenderList()}
+                    selectedItem={this.state.searchData.interestedInGender}
+                    displayKey={"name"}
+                    valueKey={"value"}
+                    onChange={this.onChange}
+                  />
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="form-row">
-            <div id="betweenAges-min" className="between-ages">
-              <Select
-                items={getAgeObjects(18, 99)}
-                name={"ageMin"}
-                displayKey={"name"}
-                valueKey={"value"}
-                selectedItem={this.state.searchData.ageMin}
-                onChange={this.onChange}
-              />
-            </div>
-            {strings.quickSearch.and}
-            <div id="betweenAges-max" className="between-ages">
-              <Select
-                items={getAgeObjects(18, 99)}
-                name={"ageMax"}
-                displayKey={"name"}
-                valueKey={"value"}
-                selectedItem={this.state.searchData.ageMax}
-                onChange={this.onChange}
-              />
+          <div>
+            <div className="form-row mb-5">
+              <div className="column column-half">
+                <div className="label">
+                  <label htmlFor="betweenAges-min">
+                    {strings.quickSearch.intrestedInAge}
+                  </label>
+                </div>
+              </div>
+
+              <div className="form-row">
+                <div id="betweenAges-min" className="between-ages">
+                  <Select
+                    items={getAgeObjects(18, 99)}
+                    name={"ageMin"}
+                    displayKey={"name"}
+                    valueKey={"value"}
+                    selectedItem={this.state.searchData.ageMin}
+                    onChange={this.onChange}
+                  />
+                </div>
+                {strings.quickSearch.and}
+                <div id="betweenAges-max" className="between-ages">
+                  <Select
+                    items={getAgeObjects(18, 99)}
+                    name={"ageMax"}
+                    displayKey={"name"}
+                    valueKey={"value"}
+                    selectedItem={this.state.searchData.ageMax}
+                    onChange={this.onChange}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="form-row center">
-          <a onClick={this.doSearch} className="btn  btn-red wide uppercase ">
-            {strings.quickSearch.search}
-          </a>
-        </div>
+          <div>
+            <div className="form-row center">
+              <a
+                onClick={this.doSearch}
+                className="btn  btn-red wide uppercase "
+              >
+                {strings.quickSearch.search}
+              </a>
+            </div>
 
-        <div className="form-row center">
-          <Link
-            to={"/registration"}
-            className="btn  btn-blue wide uppercase center"
-          >
-            {strings.quickSearch.signUp}
-          </Link>
-        </div>
+            <div className="form-row center">
+              <Link
+                to={"/registration"}
+                className="btn  btn-blue wide uppercase center"
+              >
+                {strings.quickSearch.signUp}
+              </Link>
+            </div>
+            <div className="form-row">
+              <div className="column not-a-member ">
+                <div className="space">{strings.quickSearch.signUpWith}</div>
 
-        <div className="form-row">
-          <div className="column not-a-member ">
-            <div className="space">{strings.quickSearch.signUpWith}</div>
-
-            <a href="#">
-              <i className="fab fa-google-plus-g i space gray-text"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-twitter i space gray-text"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-facebook-f i space gray-text"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-instagram space gray-text"></i>
-            </a>
+                <a href="#">
+                  <i className="fab fa-google-plus-g i space gray-text"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-twitter i space gray-text"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-facebook-f i space gray-text"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-instagram space gray-text"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </form>
@@ -165,7 +175,7 @@ function mapDispatchToProps(dispatch) {
     {
       showLoader: Actions.showLoader,
       hideLoader: Actions.hideLoader,
-      setFilterData: Actions.setData,
+      setFilterData: Actions.setData
     },
     dispatch
   );
@@ -173,7 +183,7 @@ function mapDispatchToProps(dispatch) {
 
 function mapStateToProps({ menuReducers, authReducers, filterReducers }) {
   return {
-    filter: filterReducers,
+    filter: filterReducers
   };
 }
 
